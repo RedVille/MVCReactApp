133803c1e995ef5b7970dcf389d9c2003c07e2a9
yanet <yanet@REDVILLE>
2023-11-25 10:54:09 -0600

    Agregar archivos de proyecto.

---
+++ MVCReactApp.Server/Controllers/EmpleadosController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MVCReactApp1.Models;
+
+namespace MVCReactApp.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmpleadosController : Controller
+    {
+        private readonly ReactMvcContext _context;
+
+        public EmpleadosController(ReactMvcContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Empleados
+        [HttpGet]
+        public async Task<List<Empleado>> Index()
+        {
+            List<Empleado> lista = await _context.Empleados.ToListAsync();
+            return lista;
+        }
+    }
+}
